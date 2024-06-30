feeaf5db666b0de967fc1de5d2fb5840b2e46aed
diogo-adao <diogoadao01x@gmail.com>
2024-07-01 00:20:56 +0100

    almost level 3

--- Level 3/str_capitalizer/str_capitalizer.c
+++ Level 3/str_capitalizer/str_capitalizer.c
@@ -23,21 +23,19 @@ int main(int argc, char *argv[])
 			j = 0;
 			while (argv[i][j])
 			{
-				c = argv[i][j];
-				if (argv[i][j + 1] == ' ' || argv[i][j + 1] == '\0')
+				c = ft_lower(argv[i][j]);
+				if (j == 0 && argv[i][j] >= 'a' && argv[i][j] <= 'z')
+					c = argv[i][j] - 32;
+				else if (argv[i][j] == ' ' && argv[i][j + 1] != ' ')
 				{
-					if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-						c = argv[i][j] - 32;
 					write(1, &c, 1);
 					j++;
+					c = argv[i][j];
+					if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+						c = argv[i][j] - 32;
 				}
-				if (argv[i][j] != '\0')
-				{
-					int d = ft_lower(argv[i][j]);
-					write(1, &d, 1);
-					j++;
-				} else
-					break;
+				write(1, &c, 1);
+				j++;
 			}
 			write(1, "\n", 1);
 			i++;
